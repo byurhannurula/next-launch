004b1c4d770b3cb66ec1e350d3f92f6f18f1f5d8
Byurhan Beyzat <imbyurhan@gmail.com>
2020-02-10 14:10:10 +0200

    Add header component and set home page bg's

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Head from 'next/head'
 
+import Header from '../components/header'
+
 import '../styles/app.css'
 
 function MyApp({ Component, pageProps }) {
@@ -10,7 +12,10 @@ function MyApp({ Component, pageProps }) {
         <title>Next &ndash; Launch</title>
         <meta name="title" content="Next &ndash; Launch" />
       </Head>
-      <Component {...pageProps} />
+      <Header />
+      <main role="main">
+        <Component {...pageProps} />
+      </main>
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,22 @@
 import React from 'react'
 
-const Home = () => <h1>Welcome to Next.js!</h1>
+const Home = () => (
+  <div className="bg-1">
+    <div className="container">
+      <h1>Welcome to SpaceX!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, omnis
+        recusandae. Facere, saepe. Minima, perspiciatis fugiat? Enim eligendi
+        laboriosam, quis non reiciendis fugit et cupiditate rem incidunt natus.
+        Quae, ullam?
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum,
+        ducimus aut enim doloremque minus quasi odit soluta sint, illum pariatur
+        obcaecati dolore quae sit omnis odio? Culpa iusto sed obcaecati.
+      </p>
+    </div>
+  </div>
+)
 
 export default Home
